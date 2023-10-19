225484e10860b575b140dd75f7c552ef75a702c8
SerbyRo <serbanandrei.ionescu@gmail.com>
2023-10-19 19:33:48 +0300

    Added Controller

---
+++ Ionescu_Serban_Andrei_lab1/Controllers/SerbyGreetController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ionescu_Serban_Andrei_lab1.Controllers
+{
+    public class SerbyGreetController : Controller
+    {
+        public String Index()
+        {
+            return "Welcome to my app!";
+           // return Greet();
+          //  return displayIntegerAndStringValues(22,"Heey it's Serban");
+        }
+
+        public String Greet()
+        {
+            return "Hello user!";
+        }
+
+        public String displayIntegerAndStringValues(int displayedInt, String displayedString)
+        {
+            return "These are the parameters: " + displayedInt + " and string" + displayedString;
+        }
+    }
+}
